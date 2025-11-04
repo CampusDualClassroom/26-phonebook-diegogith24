ba72b8d2035ef2f41aded6c4610bcc36ac94f76e
Diego Armando Castillo Montoya <castillodiego2401@gmail.com>
2025-11-04 20:43:35 +0100

    Solved Exercise 26

--- src/main/java/com/campusdual/classroom/Contact.java
+++ src/main/java/com/campusdual/classroom/Contact.java
@@ -1,5 +1,94 @@
 package com.campusdual.classroom;
 
-public class Contact{
+import java.text.Normalizer;
 
+
+public class Contact implements ICallActions {
+    private String name;
+    private String surName;
+    private String phoneNumber;
+
+
+    public Contact(String name, String surName, String phoneNumber) {
+        this.name = name;
+        this.surName = surName;
+        this.phoneNumber = phoneNumber;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurnames() {
+        return surName;
+    }
+
+    public void setSurnames(String lastName) {
+        this.surName = surName;
+    }
+
+    public String getPhone() {
+        return phoneNumber;
+    }
+
+    public void setPhone(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getCode(){
+        String nameClean = cleanCode(this.name);
+        String surNameClean = cleanCode(this.surName);
+
+        String[] surNames = surNameClean.split(" ");
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(nameClean.charAt(0));
+
+        if (surNames.length > 1) {
+            sb.append(surNames[0].charAt(0));
+            for (int i = 1; i < surNames.length; i++) {
+                sb.append(surNames[i]);
+            }
+        } else {
+            sb.append(surNameClean);
+        }
+        return sb.toString();
+    }
+
+    public String cleanCode(String nameCode){
+        nameCode = Normalizer.normalize(nameCode, Normalizer.Form.NFD);
+        nameCode = nameCode.replaceAll("[\\p{InCombiningDiacriticalMarks}]", "");
+        nameCode = nameCode.toLowerCase();
+        return nameCode;
+    }
+
+
+
+    @Override
+    public void callMyNumber() {
+        System.out.println("El contacto " + this.name + " " + this.surName + " se está llamando a sí mismo.");
+        System.out.println("Su número de teléfono es: " + this.phoneNumber);
+
+    }
+
+    @Override
+    public void callOtherNumber(String number) {
+        System.out.println("El contacto " + this.name + " " + this.surName + " está llamando al número: " + number);
+
+    }
+
+    @Override
+    public void showContactDetails() {
+        System.out.println("--- Detalles del Contacto ---");
+        System.out.println("Nombre Completo: " + this.name + " " + this.surName);
+        System.out.println("Número de Teléfono: " + this.phoneNumber);
+        System.out.println("Codigo: " + this.getCode());
+        System.out.println("---------------------------");
+
+    }
 }
--- src/main/java/com/campusdual/classroom/Exercise.java
+++ src/main/java/com/campusdual/classroom/Exercise.java
@@ -1,7 +1,64 @@
 package com.campusdual.classroom;
 
+
+import com.campusdual.Utils;
+
 public class Exercise {
     public static void main(String[] args) {
+        Contact c1 = new Contact("Javier", "López", "654321321");
+        Contact c2 = new Contact("Carlos", "Fernández-Simón", "654321321");
+        Contact c3 = new Contact("Jose Manuel", "Soria", "654321321");
+        Contact c4 = new Contact("Santiago","Fernández Rocha", "654321321");
+        Contact c5 = new Contact("Esteban","Serrano del Río", "654321321");
+        Contact c6 = new Contact("Fernando Miguel","Juan de los Santos Requejo León", "654321321");
 
+        Phonebook p = new Phonebook();
+
+        p.addContact(c1);
+        p.addContact(c2);
+        p.addContact(c3);
+        p.addContact(c4);
+        p.addContact(c5);
+        p.addContact(c6);
+
+        int option;
+
+        do {
+            System.out.println("\n=======   Hola. Bienvenido!   =======");
+            System.out.println("Por favor, escoge la opcion que quieres: ");
+            System.out.println("\nSelecciona 1 para añadir un nuevo contacto");
+            System.out.println("Selecciona 2 para ver tu lista de contactos");
+            System.out.println("Selecciona 3 para eliminar un contacto");
+            System.out.println("Selecciona 4 para ver un contacto");
+            System.out.println("Selecciona 5 para salir del Menú");
+            option = Utils.integer("\nPor favor, escoge una opción: ");
+
+            switch (option) {
+                case 1:
+                    Contact c = new Contact(Utils.string("Ingresa tu nombre(s): "), Utils.string("Ingresa tu apellido(s): "),
+                            Utils.string("Ingresa tu numero telefonico: "));
+
+                    p.addContact(c);
+                    break;
+                case 2:
+                    p.showPhonebook();
+                    break;
+                case 3:
+                    p.deleteContact(Utils.string("Por favor, ingresa el codigo del contacto: "));
+                    break;
+                case 4:
+                    p.getContact(Utils.string("Por favor, ingresa el codigo del contacto: "));
+                    break;
+                case 5:
+                    System.out.println("Adios!");
+                    break;
+                default:
+                    System.out.println("Tu opcion no es valida, selecciona una opcion valida: ");
+                    break;
+            }
+        } while (option != 5);
     }
 }
+
+
+
